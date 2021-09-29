a5bb5f4bf7a62e85b6ee1f37587b20d86059f33a
mathiasmagnusson <mathiasmagnussons@gmail.com>
2021-09-30 00:09:26 +0200

    Add chess

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,8 +1,13 @@
 use bevy::{
     diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     prelude::*,
+    render::camera::Camera,
+    ui::FocusPolicy,
 };
-use chess_engine::{board::SquareSpec, game::Game};
+use chess_engine::{
+    board::{SquareDiff, SquareSpec},
+    game::Game,
+};
 use std::collections::{HashMap, HashSet};
 
 fn main() {
@@ -18,6 +23,7 @@ fn main() {
         // Resources
         .insert_resource(Game::new())
         .init_resource::<PieceAssetMap>()
+        .insert_resource(PickedUpPiece(None))
         // Event types
         .add_event::<BoardUpdateEvent>()
         // Startup systems
@@ -29,6 +35,8 @@ fn main() {
         .add_system(show_diagnostics.system())
         .add_system(square_state_color.system())
         .add_system(pick_up_piece.system())
+        .add_system(put_down_piece.system())
+        .add_system(move_picked_up_piece_to_cursor.system())
         //
         .run();
 }
@@ -38,22 +46,132 @@ struct PieceSprite;
 struct BoardUpdateEvent;
 struct DiagnosticsInfoText;
 struct PickedUpPiece(Option<Entity>);
+struct PickedUpPieceParent(Entity);
 
 #[derive(Clone, Copy)]
 enum ChessSquare {
     Normal,
     Movable,
     Capturable,
+    Castlable,
+}
+
+fn move_picked_up_piece_to_cursor(
+    picked_up_piece_parent: Res<PickedUpPieceParent>,
+    mut picked_up_piece_parent_query: Query<&mut Style>,
+    windows: Res<Windows>,
+    cam_query: Query<&Transform, With<Camera>>,
+) {
+    let window = windows.get_primary().unwrap();
+
+    if let Some(pos) = window.cursor_position() {
+        let window_height = window.height();
+        let side_lenght = window_height * 0.8 / 8.0;
+
+        let cam_tranform = cam_query.single().unwrap();
+        let pos = cam_tranform.compute_matrix() * pos.extend(0.0).extend(1.0);
+        let pos = Vec2::new(pos.x, pos.y);
+
+        let mut style = picked_up_piece_parent_query
+            .get_mut(picked_up_piece_parent.0)
+            .unwrap();
+
+        style.position.left = Val::Px(pos.x - side_lenght / 2.0);
+        style.position.bottom = Val::Px(pos.y - side_lenght / 2.0);
+        style.size = Size {
+            width: Val::Px(side_lenght),
+            height: Val::Px(side_lenght),
+        }
+    }
 }
 
 fn pick_up_piece(
-    mut query: Query<(&Interaction, &SquareSpec), Changed<Interaction>>,
+    mut commands: Commands,
+    query: Query<(Entity, &Interaction, &SquareSpec), (Changed<Interaction>, With<PieceSprite>)>,
+    mut fp_query: Query<&mut FocusPolicy, With<PieceSprite>>,
     chess_game: Res<Game>,
+    mut picked_up_piece: ResMut<PickedUpPiece>,
+    picked_up_piece_parent: Res<PickedUpPieceParent>,
 ) {
+    if picked_up_piece.0.is_some() {
+        return;
+    }
+
+    for (entity, &interaction, &sq_spec) in query.iter() {
+        if interaction != Interaction::Clicked {
+            continue;
+        }
+        if Some(chess_game.current_board().turn())
+            != chess_game.current_board()[sq_spec].map(|p| p.color)
+        {
+            continue;
+        }
+        for mut focus_p in fp_query.iter_mut() {
+            *focus_p = FocusPolicy::Pass;
+        }
+        commands
+            .entity(entity)
+            .remove::<Parent>()
+            .insert(Parent(picked_up_piece_parent.0));
+        *picked_up_piece = PickedUpPiece(Some(entity));
+    }
+}
+
+fn put_down_piece(
+    query: Query<(&Interaction, &SquareSpec), With<ChessSquare>>,
+    mut picked_up_piece: ResMut<PickedUpPiece>,
+    mut picked_up_piece_query: Query<&mut SquareSpec, Without<ChessSquare>>,
+    mut chess_game: ResMut<Game>,
+    mut board_update_event: EventWriter<BoardUpdateEvent>,
+) {
+    let piece = match picked_up_piece.0 {
+        Some(p) => p,
+        None => return,
+    };
+    let piece_sq = picked_up_piece_query.get_mut(piece).unwrap();
+    let mut target = None;
     for (&interaction, &sq_spec) in query.iter() {
         if interaction == Interaction::Clicked {
-            println!("{}", sq_spec);
+            target = Some(sq_spec);
         }
+    }
+    let sq = match target {
+        Some(t) => t,
+        None => return,
+    };
+
+    let mut c = false;
+
+    if sq == *piece_sq
+        || chess_game
+            .make_move(chess_engine::board::Move::Normal {
+                from: *piece_sq,
+                to: sq,
+            })
+            .is_some()
+        || sq - *piece_sq == SquareDiff::new(0, -2)
+            && chess_game.current_board()[*piece_sq].map(|p| p.piece)
+                == Some(chess_engine::piece::PieceType::King)
+            && chess_game
+                .make_move(chess_engine::board::Move::Castling(
+                    chess_engine::board::Castling::Long,
+                ))
+                .is_some()
+        || sq - *piece_sq == SquareDiff::new(0, 2)
+            && chess_game.current_board()[*piece_sq].map(|p| p.piece)
+                == Some(chess_engine::piece::PieceType::King)
+            && chess_game
+                .make_move(chess_engine::board::Move::Castling(
+                    chess_engine::board::Castling::Short,
+                ))
+                .is_some()
+    {
+        c = true;
+    }
+
+    if c {
+        picked_up_piece.0 = None;
+        board_update_event.send(BoardUpdateEvent);
     }
 }
 
@@ -90,6 +208,7 @@ fn possible_moves_hover(
         None => return,
     };
     let moves = chess_game.current_board().get_legal_moves(hovered);
+    let moves: HashSet<chess_engine::board::Move> = moves.into_iter().collect();
     let destinations: HashSet<SquareSpec> = moves
         .iter()
         .filter_map(|m| match m {
@@ -105,6 +224,16 @@ fn possible_moves_hover(
             } else {
                 *chess_square = ChessSquare::Movable;
             }
+        } else if (sq_spec - hovered) == SquareDiff::new(0, -2)
+            && moves.contains(&chess_engine::board::Move::Castling(
+                chess_engine::board::Castling::Long,
+            ))
+            || (sq_spec - hovered) == SquareDiff::new(0, 2)
+                && moves.contains(&chess_engine::board::Move::Castling(
+                    chess_engine::board::Castling::Short,
+                ))
+        {
+            *chess_square = ChessSquare::Castlable;
         }
     }
 }
@@ -119,10 +248,11 @@ fn square_state_color(
         let color = match (is_white, chess_square) {
             (true, ChessSquare::Normal) => Color::rgb_u8(50, 50, 50),
             (false, ChessSquare::Normal) => Color::rgb_u8(40, 40, 40),
-            (true, ChessSquare::Movable) => Color::rgb_u8(0xd0, 0x87, 0x70),
-            (false, ChessSquare::Movable) => Color::rgb_u8(0xbf, 0x61, 0x6a),
-            (true, ChessSquare::Capturable) => Color::rgb_u8(0xeb, 0xcb, 0x8b),
-            (false, ChessSquare::Capturable) => Color::rgb_u8(0xeb, 0xcb, 0x8b),
+            (true, ChessSquare::Capturable) => Color::rgb_u8(0xd0, 0x87, 0x70),
+            (false, ChessSquare::Capturable) => Color::rgb_u8(0xbf, 0x61, 0x6a),
+            (true, ChessSquare::Movable) => Color::rgb_u8(0xdb, 0xbb, 0x7b),
+            (false, ChessSquare::Movable) => Color::rgb_u8(0xca, 0xa1, 0x75),
+            (_, ChessSquare::Castlable) => Color::rgb_u8(0x8f, 0xbc, 0xbb),
         };
         *material = materials.add(color.into());
     }
@@ -195,12 +325,14 @@ fn assign_square_sprites(
                         .spawn_bundle(NodeBundle {
                             style: Style {
                                 size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                                position_type: PositionType::Absolute,
                                 ..Default::default()
                             },
                             material: asset_map.0.get(&piece).unwrap().clone(),
                             ..Default::default()
                         })
                         .insert(Interaction::default())
+                        .insert(FocusPolicy::Block)
                         .insert(sq_spec.clone())
                         .insert(PieceSprite);
                 });
@@ -215,6 +347,7 @@ fn setup_game_ui(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let mut picked_up_piece_parent = Entity::new(0);
     commands.spawn_bundle(UiCameraBundle::default());
     commands
         .spawn_bundle(NodeBundle {
@@ -231,7 +364,7 @@ fn setup_game_ui(
             root.spawn_bundle(NodeBundle {
                 style: Style {
                     size: Size::new(Val::Undefined, Val::Percent(80.0)),
-                    aspect_ratio: Some(0.8),
+                    aspect_ratio: Some(0.8), // i dont know why but this makes it a square
                     ..Default::default()
                 },
                 material: materials.add(Color::rgb_u8(40, 40, 40).into()),
@@ -303,12 +436,27 @@ fn setup_game_ui(
                                 },
                                 ..Default::default()
                             })
+                            .insert(Interaction::default())
                             .insert(SquareSpec::new(rank, file))
                             .insert(ChessSquare::Normal);
                     }
                 }
             });
+            let e = root
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        position_type: PositionType::Absolute,
+                        ..Default::default()
+                    },
+                    material: materials.add(Color::NONE.into()),
+                    ..Default::default()
+                })
+                .insert(FocusPolicy::Pass)
+                .id();
+            picked_up_piece_parent = e;
         });
+
+    commands.insert_resource(PickedUpPieceParent(picked_up_piece_parent));
 
     board_update_event.send(BoardUpdateEvent);
 }
